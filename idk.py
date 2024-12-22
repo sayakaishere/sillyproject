f62558fc5d9f84ac9872cb79315f5668cd0f93e1
sayaka <156920097+sayakaishere@users.noreply.github.com>
2024-12-23 02:44:36 +0700

    idk.py

---
+++ idk.py
@@ -0,0 +1,15 @@
+import tkinter as tk
+
+def on_button_click():
+    print("Button clicked LOL!")
+
+# Create the main window
+root = tk.Tk()
+root.title("IDK WHY")
+
+# Create a button widget
+button = tk.Button(root, text="Click Me Bitch", command=on_button_click)
+button.pack()
+
+# Start the main event loop
+root.mainloop()
